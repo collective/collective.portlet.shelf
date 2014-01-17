a342c59d36746f04d2b4f36dfc013f50dc0b6cb4
Roman Kozlovskyi <kroman0@users.noreply.github.com>
2014-01-17 12:27:08 +0200

    Update shelf.js

--- collective/portlet/shelf/browser/resources/shelf.js
+++ collective/portlet/shelf/browser/resources/shelf.js
@@ -1,52 +1,55 @@
-jq(document).ready(function () {
-    var shelfs = jq('.shelf');
-    shelfs.each( function(index) {
+jQuery(document).ready(function ($) {
+    var shelfs = $('.shelf');
+    shelfs.each(function (index) {
         var shelf = this;
-        var cat_controls = jq(shelf).find('.subCategory');
+        var cat_controls = $(shelf).find('.subCategory');
         cat_controls.click(function (e) {
             e.preventDefault();
-            var classes = jq(this).attr('class').split(' ');
+            var classes = $(this).attr('class').split(' ');
             var category_id = '';
-            for (i=0; i<classes.length; i++){
+            for (i = 0; i < classes.length; i++) {
                 if (classes[i].search('shelfc') != -1) {
                     category_id = classes[i];
                 }
             }
-            jq(shelf).find('div.'+category_id).show();
+            $(shelf).find('div.' + category_id).show();
             initControls();
-            var sections = jq(shelf).find(".category_section");
-            sections.each( function(idx) {
-                if (jq(this).attr('class').search(category_id) === -1){
-                    jq(this).hide();
+            var sections = $(shelf).find(".category_section");
+            sections.each(function (idx) {
+                if ($(this).attr('class').search(category_id) === -1) {
+                    $(this).hide();
                 }
             });
-    });
+        });
     });
 
-    var initControls = function(){
-        var sections = jq(".category_section");
-        sections.each( function(i) {
-            var section= this;
-            var scrollable = jq(section).find(".shelfScrollable");
-            var scrollable_width = jq(scrollable).width();         
-            if (scrollable_width !=0) {
-                var elems = jq(scrollable).find('.scrollItem');
+    var initControls = function () {
+        var sections = $(".category_section");
+        sections.each(function (i) {
+            var section = this;
+            var scrollable = $(section).find(".shelfScrollable");
+            var scrollable_width = $(scrollable).width();
+            if (scrollable_width != 0) {
+                var elems = $(scrollable).find('.scrollItem');
                 var all_elems_width = 0;
-                for (i=0; i<elems.length; i++) {   
-                    all_elems_width = all_elems_width + jq(elems[i]).width();
+                for (i = 0; i < elems.length; i++) {
+                    all_elems_width = all_elems_width + $(elems[i]).width();
                 };
                 if (scrollable_width > all_elems_width) {
-                    jq(section).find('.browse, .scrollNavi').hide();
+                    $(section).find('.browse, .scrollNavi').hide();
 
                 }
-           }; 
+            };
         });
     };
 
     initControls();
-    var api = jq("div.shelfScrollable").scrollable({
+    var api = $("div.shelfScrollable").scrollable({
         size: 1,
         clickable: false,
         loop: true
-    }).navigator({indexed: true, navi: '.scrollNavi'}).pager();
+    }).navigator({
+        indexed: true,
+        navi: '.scrollNavi'
+    }).pager();
 });
